38e0fadcce405f21d30ffd75f841cc4cdb6adbd0
Rotshidzwa Nematswerani <gtrochie@gmail.com>
2026-09-03 10:44:14 +0200

    done with MergeArrays and RemoveDuplicates

--- methods/src/main/java/dsa/methods/MergeArrays.java
+++ methods/src/main/java/dsa/methods/MergeArrays.java
@@ -1,7 +1,25 @@
 package dsa.methods;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MergeArrays {
+
     public static int[] merge(int[] a, int[] b) {
-        // TODO
-        return new int[]{};
+        List<Integer> merge = new ArrayList<>();
+        for (int num : a){
+            merge.add(num);
+        }
+        for (int num : b){
+            merge.add(num);
+        }
+
+        int[] merged = new int[merge.size()];
+        int index = 0;
+        for (int num : merge){
+            merged[index++] = num;
+
+        }
+        return merged;
     }
 }
--- methods/src/main/java/dsa/methods/RemoveDuplicates.java
+++ methods/src/main/java/dsa/methods/RemoveDuplicates.java
@@ -1,7 +1,22 @@
 package dsa.methods;
+import java.util.Set;
+import java.util.LinkedHashSet;
+
 public class RemoveDuplicates {
     public static int[] removeDuplicates(int[] arr) {
-        // TODO
-        return new int[]{};
-    }
+        if (arr == null){
+            return new int[0];
+        }
+        Set<Integer> removedDuplicates = new LinkedHashSet<>();
+        for (int num : arr){
+            removedDuplicates.add(num);
+        }
+        int[] noDuplicates = new int[removedDuplicates.size()];
+        int index = 0;
+        for (int num : removedDuplicates){
+            noDuplicates[index++] = num;
+
+        }
+        return noDuplicates;
+}
 }
