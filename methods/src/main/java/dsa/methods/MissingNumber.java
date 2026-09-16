c7e391aa36f8bd885c2487052e52d53bdb7c2b0a
Rotshidzwa Nematswerani <gtrochie@gmail.com>
2026-09-16 14:19:20 +0200

    done with methods

--- methods/src/main/java/dsa/methods/MissingNumber.java
+++ methods/src/main/java/dsa/methods/MissingNumber.java
@@ -2,7 +2,13 @@ package dsa.methods;
 public class MissingNumber {
     // array contains 1..n with one number missing
     public static int findMissing(int[] arr, int n) {
-        // TODO
-        return 0;
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+
+        for (int num : arr){
+            actualSum += num;
+        }
+
+        return expectedSum -  actualSum;
     }
 }
--- methods/src/main/java/dsa/methods/RotateArray.java
+++ methods/src/main/java/dsa/methods/RotateArray.java
@@ -1,8 +1,18 @@
 package dsa.methods;
+
+import java.util.Arrays;
+
 public class RotateArray {
     // rotate left by k positions
     public static int[] rotate(int[] arr, int k) {
-        // TODO
-        return new int[]{};
+       int n = arr.length;
+       int [] result = new int[n];
+       k = k % n;
+       for (int i = 0; i < n; i++){
+           result[i] = arr[(i + k) % n];
+       }
+
+
+        return result;
     }
 }
